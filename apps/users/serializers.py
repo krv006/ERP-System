5d34cd53303b7c1d33b00548748561645f5d2676
krv006 <rvkamronbek@gmail.com>
2025-06-17 13:54:05 +0500

    password change

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.hashers import make_password
 from django.core.cache import cache
 from rest_framework.exceptions import ValidationError
-from rest_framework.fields import CharField, EmailField
+from rest_framework.fields import CharField, EmailField, HiddenField, CurrentUserDefault
 from rest_framework.serializers import ModelSerializer, Serializer
 
 from users.gen_code import generate_code
@@ -38,7 +38,7 @@ class RegisterUserModelSerializer(ModelSerializer):
         return user
 
 
-class LoginUserModelSerializer(ModelSerializer):
+class LoginUserModelSerializer(Serializer):
     email = EmailField()
     password = CharField(write_only=True)
 
@@ -48,10 +48,10 @@ class LoginUserModelSerializer(ModelSerializer):
         try:
             user = User.objects.get(email=email)
         except User.DoesNotExist:
-            raise ValidationError("Invalid email or password")
+            raise ValidationError("Bunday email mavjud emas bizda")
 
         if not user.check_password(password):
-            raise ValidationError("Invalid email or password")
+            raise ValidationError("Password ni xato kiritdingiz !!!")
         if not user.is_active:
             user.is_active = True
             user.save(update_fields=['is_active'])
@@ -77,6 +77,17 @@ class VerifyCodeSerializer(Serializer):
         user.save()
         return user
 
+# class ChangeUserPasswordSerializer(Serializer):
+#     email = EmailField()
+#     new_password = CharField(write_only=True)
+#
+#     def validate(self, attrs):
+#         email = attrs.get('email')
+#         try:
+#             user = User.objects.get(email=email)
+#         except User.DoesNotExist:
+#             raise ValidationError("Bunday email mavjud emas bizda")
+
 
 class UserModelSerializer(ModelSerializer):
     class Meta:
@@ -94,3 +105,78 @@ class UserDetailModelSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = 'id', 'username', 'email', 'role', 'first_name', 'last_name',
+
+# todo ChangePassword
+
+# class RessetVerifyCodeSerializer(Serializer):
+#     email = EmailField()
+#
+#     def validate(self, attrs):
+#         attrs = super().validate(attrs)
+#         email = attrs.get('email')
+#         if cache.get(f"{email}_verification"):
+#             raise ValidationError('Your old password is active still!')
+#         code = generate_code()
+#         cache.set(f"{email}_verification", code, timeout=120)
+#         send_verification_email(email, code)
+#         return attrs
+#
+#
+# class ResetPasswordSerializer(Serializer):
+#     email = EmailField(required=False, allow_blank=True, allow_null=True)
+#     phone = CharField(required=False, allow_blank=True, allow_null=True)
+#     user = HiddenField(default=CurrentUserDefault())
+#
+#     def validate(self, attrs):
+#         attrs = super().validate(attrs)
+#         email = attrs.get('email')
+#         phone_number = attrs.get('phone')
+#         if not email and not phone_number:
+#             raise ValidationError("Email or phone on of them are required")
+#         if email:
+#             if not User.objects.filter(email=email).exists():
+#                 raise ValidationError(f"No such user with email: {email}")
+#             code = generate_code()
+#             cache.set(f"{email}_verification", code, timeout=120)
+#             send_verification_email(email, code)
+#         if phone_number:
+#             if not User.objects.filter(phone_number=phone_number).exists():
+#                 raise ValidationError(f"No such user with phone number: {phone_number}")
+#             code = generate_code()
+#             cache.set(f"{phone_number}_verification", code, timeout=120)
+#             send_verification_phone(phone_number, code)
+#         return attrs
+#
+#
+# class ConfirmPasswordSerializer(Serializer):
+#     email = EmailField(required=False, allow_blank=True, allow_null=True)
+#     phone_number = CharField(required=False, allow_blank=True, allow_null=True)
+#     password = CharField()
+#     confirm_password = CharField()
+#     code = CharField()
+#
+#     def validate(self, attrs):
+#         attrs = super().validate(attrs)
+#         email = attrs.get("email")
+#         code = attrs.pop("code")
+#         phone_number = attrs.get("phone_number")
+#         password = attrs.get("password")
+#         confirm_password = attrs.pop("confirm_password")
+#         gen_code_email = cache.get(f'{email}_verification')
+#         gen_code_phone = cache.get(f'{phone_number}_verification')
+#         if email:
+#             if gen_code_email is None:
+#                 raise ValidationError("Your verification already expired!")
+#             if code != gen_code_email:
+#                 raise ValidationError("Code didn't matched")
+#         if phone_number:
+#             if gen_code_phone is None:
+#                 raise ValidationError("Your verification already expired!")
+#             if code != gen_code_phone:
+#                 raise ValidationError("Code didn't matched")
+#         if password != confirm_password:
+#             raise ValidationError("Passwords didn't matched!")
+#         instance = User.objects.get(email=email) if email else User.objects.get(phone_number=phone_number)
+#         instance.set_password(password)
+#         instance.save()
+#         return instance
